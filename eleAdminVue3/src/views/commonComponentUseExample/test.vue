bf83086e17d74aa429005fcb7eb154b0f1c9dcba
huang.yx <1226817943@qq.com>
2023-06-15 18:41:27 +0800

    feat:使用复选框实现单选框的效果

--- eleAdminVue3/src/views/commonComponentUseExample/test.vue
+++ eleAdminVue3/src/views/commonComponentUseExample/test.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-row :gutter="10">
+  <!-- <el-row :gutter="10">
     <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
       <div class="grid-content ep-bg-purple"></div>
     </el-col>
@@ -14,11 +14,47 @@
     <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
       <div class="grid-content ep-bg-purple-light"></div>
     </el-col>
-  </el-row>
+  </el-row> -->
+
+  <div>
+    <p>使用checkbox实现单选效果{{ curSelItem }}</p>
+    <el-checkbox-group v-model="curSelItem" @change="handleCheckedChange">
+      <el-checkbox
+        v-for="(item, index) in listData"
+        :key="item.id"
+        :label="item.id"
+        >{{ item.name }}</el-checkbox
+      >
+    </el-checkbox-group>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive, onMounted } from "vue";
+const curSelItem = ref<number[]>([]); //checkbox-group 绑定的值
+const prevSelItem = ref(); //前一个选择的id
+const listData = ref([
+  { id: 1, name: "语文" },
+  { id: 2, name: "数学" },
+  { id: 3, name: "英语" },
+]);
+// if (listData.value.length) {
+//   curSelItem.value = [listData.value[0].id];
+// }
+const handleCheckedChange = (value: string[] | number[]) => {
+  console.log("value", value);
+  const list = [...value] as number[];
+  console.log("list", list);
+  const len = list.length;
+  const newItem = list[len - 1]; //获取数组最后一位的数据
+  // console.log("newItem", newItem);
+  if (newItem) {
+    curSelItem.value = [newItem];
+    prevSelItem.value = newItem;
+  } else {
+    curSelItem.value = [prevSelItem.value];
+  }
+};
 </script>
 
 <style lang="scss" scoped>
